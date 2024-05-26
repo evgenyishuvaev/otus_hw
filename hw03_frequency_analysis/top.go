685cb4452572cc98371b4c34d56c7248754ae3e4
Evgenyi Shuvaev <evgenyishuvaevmac@yandex.ru>
2024-05-26 19:41:56 +0300

    HW03 is Completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,56 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+// Сортирует слайс ключей на основе кол-во вхождения в строку.
+// slcWord - слайс ключей мапы mpWord.
+// mpWord - мапа где ключ слово, а значений кол-во вхождений.
+func sortSliceByCount(slcWord []string, mpWord map[string]int) []string {
+	sort.SliceStable(slcWord, func(i, j int) bool {
+		return mpWord[slcWord[i]] > mpWord[slcWord[j]]
+	})
+	return slcWord
+}
+
+// Лексикографическая сортировка слайса с учетом кол-во вхождений.
+func finalSortSliceByLexicAndCount(slcWord []string, mpWord map[string]int) []string {
+	sort.Slice(slcWord, func(i, j int) bool {
+		if mpWord[slcWord[i]] == mpWord[slcWord[j]] {
+			return slcWord[i] < slcWord[j]
+		}
+		return mpWord[slcWord[i]] > mpWord[slcWord[j]]
+	})
+	return slcWord
+}
+
+func Top10(in string) []string {
+	if len(in) == 0 {
+		return []string{}
+	}
+
+	splitedIn := strings.Fields(in)
+	countMap := make(map[string]int)
+	for _, word := range splitedIn {
+		_, ok := countMap[word]
+		if !ok {
+			countMap[word] = 1
+		} else {
+			countMap[word]++
+		}
+	}
+
+	freqWords := make([]string, 0, len(countMap))
+
+	for key := range countMap {
+		freqWords = append(freqWords, key)
+	}
+
+	freqWords = sortSliceByCount(freqWords, countMap)
+	if len(freqWords) > 10 {
+		return finalSortSliceByLexicAndCount(freqWords[:10], countMap)
+	}
+	return finalSortSliceByLexicAndCount(freqWords, countMap)
 }
