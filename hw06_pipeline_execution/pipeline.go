8afcbb33b6b6fc2c7da54dee31e86d42d6ef5ddd
Evgenyi Shuvaev <evgenyishuvaevmac@yandex.ru>
2024-11-25 22:27:26 +0300

    черновик

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,27 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	// log.Println("Старт пайплайна")
+	var outCh Out
+	var inCh Bi = make(Bi)
+
+	go func() {
+		defer close(inCh)
+		for val := range in {
+			// log.Printf("на очереди %d", val.(int))
+			select {
+			case <-done:
+				return
+			case inCh <- val:
+				// log.Printf("Записываем в канал %d", val.(int))
+			}
+		}
+	}()
+
+	outCh = inCh
+	// outCh = in
+	for _, stage := range stages {
+		outCh = stage(outCh)
+	}
+	return outCh
 }
